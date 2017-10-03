8174476635becc7b85e39a29bb8d86d8d9d9e348
Marcus Kida <marcus@kida.io>
2017-10-03 13:11:31 +0200

    Add extension on List<Middleware<T, U>>

--- src/main/kotlin/io/kida/ktor/middleware/Middleware.kt
+++ src/main/kotlin/io/kida/ktor/middleware/Middleware.kt
@@ -7,16 +7,12 @@ interface Middleware<T: Any, out U: Any> {
     suspend fun pass(ctx: PipelineContext<T>): U?
 }
 
-class Middlewares {
-    companion object {
-        suspend fun <T: Any, U: Any>evaluate(ctx: PipelineContext<T>, middlewares: List<Middleware<T, U>>): MiddlewareResult<U> {
-            val results = middlewares.map { it.pass(ctx) }
-            val failed = results.filter { it !== null }.count()
-            return if (failed == 0) {
-                MiddlewareResult(true, null)
-            } else {
-                MiddlewareResult(false, results.first()!!)
-            }
-        }
+suspend fun <T: Any, U: Any>List<Middleware<T, U>>.evaluate(ctx: PipelineContext<T>): MiddlewareResult<U> {
+    val results = map { it.pass(ctx) }
+    val failed = results.filter { it !== null }.count()
+    return if (failed == 0) {
+        MiddlewareResult(true, null)
+    } else {
+        MiddlewareResult(false, results.first()!!)
     }
 }
