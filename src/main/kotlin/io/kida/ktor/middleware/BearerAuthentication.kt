d0a97d5e6c62b9d6744b24b32ebe32b5639b045a
Marcus Kida <marcus@kida.io>
2017-11-25 10:43:31 +0100

    Inline OK status and fix code formatting

--- src/main/kotlin/io/kida/ktor/middleware/BearerAuthentication.kt
+++ src/main/kotlin/io/kida/ktor/middleware/BearerAuthentication.kt
@@ -3,7 +3,7 @@ package io.kida.ktor.middleware
 import io.kida.ktor.middleware.model.HttpStatus
 import org.jetbrains.ktor.pipeline.PipelineContext
 
-class BearerAuthentication(private val token: String): Middleware<Unit, HttpStatus> {
+class BearerAuthentication(private val token: String) : Middleware<Unit, HttpStatus> {
     suspend override fun pass(ctx: PipelineContext<Unit>): HttpStatus? {
         if (ctx.call.request.headers["Authorization"].equals("Bearer $token")) {
             return null
--- src/main/kotlin/io/kida/ktor/middleware/model/HttpStatus.kt
+++ src/main/kotlin/io/kida/ktor/middleware/model/HttpStatus.kt
@@ -2,8 +2,6 @@ package io.kida.ktor.middleware.model
 
 data class HttpStatus(val status: Int, val message: String, val details: Any? = null) {
     companion object {
-        fun OK(details: Any? = null): HttpStatus {
-            return HttpStatus(200, "ok", details)
-        }
+        fun OK(details: Any? = null): HttpStatus = HttpStatus(200, "ok", details)
     }
 }
